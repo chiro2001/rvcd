8ac1fc5f2e511273c0e45a3ed21a61c837e42e34
Chiro Liang <Chiro2001@163.com>
2023-01-17 09:34:08 +0800

    show vcd tree

--- src/wave.rs
+++ src/wave.rs
@@ -1,22 +1,53 @@
 use std::io::Read;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use log::info;
-use vcd::Header;
+use vcd::{Header, ScopeItem};
+use vcd::ScopeItem::{Scope, Var};
 
 pub fn vcd_header_show(header: &Header) {
-    // info!("{header:#?}");
-    info!("comment: {:#?}", header.comment);
-    info!("date: {:#?}", header.date);
-    info!("version: {:#?}", header.version);
-    info!("timescale: {:#?}", header.timescale);
-    // header.items.
-    info!("done");
+    header.comment.as_ref().map(|c| info!("comment: {}", c));
+    header.date.as_ref().map(|c| info!("date: {}", c));
+    header.version.as_ref().map(|c| info!("version: {}", c));
+    header.timescale.as_ref().map(|c| info!("timescale: {} / {}", c.0, c.1));
+}
+
+pub fn vcd_tree_show(header: &Header) {
+    fn show(item: &ScopeItem, level: usize) {
+        match item {
+            Scope(scope) => {
+                println!("{}{}", (0..level).map(|_| "\t").collect::<Vec<&str>>().join(""), scope.identifier);
+                scope.children.iter().for_each(|i| show(i, level + 1));
+            }
+            Var(var) => {
+                println!("{}{} width={}",
+                         (0..level).map(|_| "\t").collect::<Vec<&str>>().join(""),
+                         var.reference, var.size);
+            }
+        }
+    }
+    header.items.iter().for_each(|item| show(item, 0));
 }
 
 pub fn vcd_read(r: &mut dyn Read) -> Result<()> {
     let mut parser = vcd::Parser::new(r);
     let header = parser.parse_header()?;
     vcd_header_show(&header);
+    let scope_item = header.items.first().ok_or(anyhow!("no root scope!"))?;
+    match scope_item {
+        Scope(scope) => {
+            info!("scope: {} {} children:", scope.identifier, scope.scope_type);
+            scope.children.iter().for_each(|item| {
+                match item {
+                    Scope(_scope) => {}
+                    Var(var) => {
+                        info!("var {}", var.reference);
+                    }
+                }
+            });
+        }
+        Var(_var) => {}
+    }
+    vcd_tree_show(&header);
     Ok(())
 }
 
