c5b56c34d24410f5727e17a7d6c7c6b6737b3232
Chiro Liang <Chiro2001@163.com>
2023-01-20 11:09:50 +0800

    update service

--- src/service.rs
+++ src/service.rs
@@ -10,16 +10,16 @@ pub async fn service(channel: RVCDChannel) {
     let tx = Arc::new(Mutex::new(channel.tx));
     loop {
         sleep(Duration::from_millis(10));
-        match rx.try_lock() {
-            Ok(rx) => match rx.try_recv() {
-                Ok(msg) => match msg {
+        rx.try_lock()
+            .map(|rx| rx.try_recv())
+            .map(|r| {
+                r.map(|msg| match msg {
                     RVCDMsg::FileOpen(path) => {
                         info!("loading file: {:?}", path);
                     }
-                },
-                Err(_) => {}
-            },
-            Err(_) => {}
-        };
+                })
+                .ok()
+            })
+            .ok();
     }
 }
