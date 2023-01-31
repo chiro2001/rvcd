8ef640fe776d694e566e26b23d584da83c281c26
Chiro Liang <Chiro2001@163.com>
2023-01-31 14:14:39 +0800

    paint rect

--- src/view.rs
+++ src/view.rs
@@ -1,5 +1,5 @@
 use crate::wave::{WaveDataItem, WaveInfo};
-use egui::{vec2, Align2, ScrollArea, Sense, Ui};
+use egui::{pos2, vec2, Align2, Color32, Rect, ScrollArea, Sense, Ui};
 
 #[derive(serde::Deserialize, serde::Serialize, Default, PartialEq)]
 #[serde(default)]
@@ -63,22 +63,39 @@ impl WaveView {
                         ui.scope(|ui| {
                             ui.set_height(signal.height);
                             ui.centered_and_justified(|ui| {
-                                let (mut _response, painter) = ui.allocate_painter(
+                                let (response, painter) = ui.allocate_painter(
                                     ui.available_size_before_wrap(),
                                     Sense::hover(),
                                 );
                                 let items = wave_data.iter().filter(|i| i.id == signal.id);
                                 let color = ui.visuals().strong_text_color();
-                                let rect = ui.max_rect();
+                                // let signal_rect = ui.max_rect();
+                                let signal_rect = response.rect;
+                                // painter.rect_stroke(signal_rect, 0.0, (2.0, Color32::GREEN));
                                 let mut it = items;
                                 let mut item_last: Option<&WaveDataItem> = None;
-                                while let Some(item) = it.next() {
-                                    if let Some(item_last) = item_last {
-                                        let text = item_last.value.to_string();
-                                        let width = rect.right() - rect.left();
-                                        let percent = ((item_last.timestamp - info.range.0) as f32)
-                                            / ((self.range.1 - self.range.0) as f32);
-                                        let pos = rect.left_center() + vec2(width * percent, 0.0);
+                                let paint_signal =
+                                    |item_now: &WaveDataItem, item_next: &WaveDataItem| {
+                                        let text = item_now.value.to_string();
+                                        let width = signal_rect.width();
+                                        let height = signal_rect.height();
+                                        let percent_rect_left = (item_now.timestamp - info.range.0) as f32
+                                            / (self.range.1 - self.range.0) as f32;
+                                        let percent_rect_right = (item_next.timestamp - info.range.0) as f32
+                                            / (self.range.1 - self.range.0) as f32;
+                                        let percent_text =
+                                            (((item_now.timestamp + item_next.timestamp) as f32
+                                                / 2.0)
+                                                - info.range.0 as f32)
+                                                / (self.range.1 - self.range.0) as f32;
+                                        let rect = Rect::from_min_max(
+                                            pos2(signal_rect.left() + width * percent_rect_left, signal_rect.top()),
+                                            pos2(signal_rect.left() + width * percent_rect_right, signal_rect.top() + height),
+                                        );
+                                        // info!("parent: {:?}, rect: {:?}", signal_rect, rect);
+                                        painter.rect_stroke(rect, 0.0, (1.2, Color32::GREEN));
+                                        let pos = signal_rect.left_center()
+                                            + vec2(width * percent_text, 0.0);
                                         painter.text(
                                             pos,
                                             Align2::CENTER_CENTER,
@@ -86,8 +103,22 @@ impl WaveView {
                                             Default::default(),
                                             color,
                                         );
+                                    };
+                                while let Some(item) = it.next() {
+                                    if let Some(item_last) = item_last {
+                                        paint_signal(item_last, item);
                                     }
                                     item_last = Some(item);
+                                }
+                                // draw last
+                                if let Some(item_last) = item_last {
+                                    paint_signal(
+                                        item_last,
+                                        &WaveDataItem {
+                                            timestamp: info.range.1,
+                                            ..WaveDataItem::default()
+                                        },
+                                    );
                                 }
                             });
                         });
