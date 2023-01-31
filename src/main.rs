cc70126cf2f7d42bcfc1b42572360b5908bb5900
Chiro Liang <Chiro2001@163.com>
2023-01-31 10:13:32 +0800

    start service native

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use anyhow::Result;
+use futures::future::join;
+use rvcd::server::server::rvcd_rpc_server::RvcdRpcServer;
+use rvcd::server::RvcdRemote;
 use rvcd::Rvcd;
+use tonic::transport::Server;
+use tracing::info;
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
 
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "Rvcd",
-        native_options,
-        Box::new(|cc| Box::new(Rvcd::new(cc))),
-    );
+    let gui = async move {
+        eframe::run_native(
+            "Rvcd",
+            native_options,
+            Box::new(|cc| Box::new(Rvcd::new(cc))),
+        );
+    };
+    let rpc = async move {
+        let addr = "[::1]:50051".parse().unwrap();
+        info!("starting rpc server at {}", addr);
+        Server::builder()
+            .add_service(RvcdRpcServer::new(RvcdRemote::default()))
+            .serve(addr)
+            .await
+            .unwrap();
+    };
+    let _ = join(gui, rpc).await;
+    Ok(())
 }
 
 // when compiling to web using trunk.
