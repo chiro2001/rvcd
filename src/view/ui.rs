c90b6e2bbe4ee52a41e439362b3bb309ae1146f0
Chiro Liang <Chiro2001@163.com>
2023-02-01 23:24:27 +0800

    split view ui

--- src/view/ui.rs
+++ src/view/ui.rs
@@ -1,13 +1,11 @@
 use crate::message::RvcdMsg;
 use crate::radix::Radix;
-use crate::view::signal::{SIGNAL_HEIGHT_DEFAULT, SignalView, SignalViewAlign, SignalViewMode};
-use crate::view::{WaveView, BG_MULTIPLY, LINE_WIDTH, MIN_SIGNAL_WIDTH, TEXT_ROUND_OFFSET, UI_WIDTH_OFFSET};
-use crate::wave::{WaveDataItem, WaveDataValue, WaveInfo, WireValue};
+use crate::view::signal::SIGNAL_HEIGHT_DEFAULT;
+use crate::view::{WaveView, BG_MULTIPLY, UI_WIDTH_OFFSET, LINE_WIDTH};
+use crate::wave::{WaveDataItem, WaveInfo};
 use egui::*;
-use num_bigint::BigUint;
-use num_traits::{One, Zero};
-use std::ops::RangeInclusive;
 use egui_extras::{Column, TableBuilder};
+use std::ops::RangeInclusive;
 use tracing::*;
 
 impl WaveView {
@@ -24,8 +22,11 @@ impl WaveView {
                 });
             });
             ui.menu_button(format!("Align: {:?}", self.align), |ui| {
-                use SignalViewAlign::*;
-                let data = [Left, Center, Right];
+                let data = [
+                    super::SignalViewAlign::Left,
+                    super::SignalViewAlign::Center,
+                    super::SignalViewAlign::Right,
+                ];
                 data.into_iter().for_each(|a| {
                     if ui.button(format!("{:?}", a)).clicked() {
                         self.align = a;
@@ -64,272 +65,13 @@ impl WaveView {
             }
         });
     }
-    fn ui_signal_wave(
-        &self,
-        signal: &SignalView,
-        wave_data: &[WaveDataItem],
-        info: &WaveInfo,
-        ui: &mut Ui,
-    ) -> Response {
-        let (response, painter) =
-            ui.allocate_painter(ui.available_size_before_wrap(), Sense::click_and_drag());
-        let items = wave_data.iter().filter(|i| i.id == signal.s.id);
-        let text_color = ui.visuals().strong_text_color();
-        let signal_rect = response.rect;
-        let mut it = items;
-        let mut item_last: Option<&WaveDataItem> = None;
-        let mut ignore_x_start = -1.0;
-        let mut ignore_has_x = false;
-        let mut paint_signal = |item_now: &WaveDataItem, item_next: &WaveDataItem| {
-            let single: bool = match &item_now.value {
-                WaveDataValue::Comp(_) => {
-                    let d = ("".to_string(), 0);
-                    let (_v, w) = info.code_name_width.get(&signal.s.id).unwrap_or(&d);
-                    *w == 1
-                }
-                WaveDataValue::Raw(v) => v.len() == 1,
-            };
-            let width = signal_rect.width();
-            let height = signal_rect.height();
-            let percent_rect_left =
-                (item_now.timestamp - info.range.0) as f32 / (self.range.1 - self.range.0) as f32;
-            let percent_rect_right =
-                (item_next.timestamp - info.range.0) as f32 / (self.range.1 - self.range.0) as f32;
-            let percent_text = (((item_now.timestamp + item_next.timestamp) as f32 / 2.0)
-                - info.range.0 as f32)
-                / (self.range.1 - self.range.0) as f32;
-            let rect = Rect::from_min_max(
-                pos2(
-                    signal_rect.left() + width * percent_rect_left,
-                    signal_rect.top(),
-                ),
-                pos2(
-                    signal_rect.left() + width * percent_rect_right,
-                    signal_rect.top() + height,
-                ),
-            );
-            let radix = match &signal.mode {
-                SignalViewMode::Default => self.default_radix.clone(),
-                SignalViewMode::Number(r) => r.clone(),
-                SignalViewMode::Analog => Radix::Hex,
-            };
-            let text = item_now.value.as_radix(radix);
-            if rect.width() > MIN_SIGNAL_WIDTH {
-                if ignore_x_start >= 0.0 {
-                    // paint a rect as ignored data
-                    painter.rect_filled(
-                        Rect::from_x_y_ranges(
-                            RangeInclusive::new(ignore_x_start, rect.left()),
-                            rect.y_range(),
-                        ),
-                        0.0,
-                        if ignore_has_x {
-                            Color32::DARK_RED
-                        } else {
-                            Color32::GREEN
-                        },
-                    );
-                    ignore_x_start = -1.0;
-                    ignore_has_x = false;
-                }
-                let paint_x = || {
-                    painter.rect(
-                        rect,
-                        0.0,
-                        if self.background {
-                            Color32::DARK_RED.linear_multiply(BG_MULTIPLY)
-                        } else {
-                            Color32::TRANSPARENT
-                        },
-                        (LINE_WIDTH, Color32::RED),
-                    )
-                };
-                let paint_z = || painter.rect_stroke(rect, 0.0, (LINE_WIDTH, Color32::DARK_RED));
-                if single {
-                    let value = match &item_now.value {
-                        WaveDataValue::Comp(v) => match BigUint::from_bytes_le(v).is_one() {
-                            true => WireValue::V1,
-                            false => WireValue::V0,
-                        },
-                        WaveDataValue::Raw(v) => v[0],
-                    };
-                    match value {
-                        WireValue::V0 => {
-                            painter.hline(
-                                rect.x_range(),
-                                rect.bottom(),
-                                (LINE_WIDTH, Color32::GREEN),
-                            );
-                            painter.vline(
-                                rect.left(),
-                                rect.y_range(),
-                                (LINE_WIDTH, Color32::GREEN),
-                            );
-                        }
-                        WireValue::V1 => {
-                            painter.hline(rect.x_range(), rect.top(), (LINE_WIDTH, Color32::GREEN));
-                            painter.vline(
-                                rect.left(),
-                                rect.y_range(),
-                                (LINE_WIDTH, Color32::GREEN),
-                            );
-                        }
-                        WireValue::X => paint_x(),
-                        WireValue::Z => paint_z(),
-                    };
-                } else {
-                    let number: Option<BigUint> = (&item_now.value).into();
-                    if text.contains('x') {
-                        paint_x();
-                    } else {
-                        if text.contains('z') {
-                            paint_z();
-                        } else {
-                            match number {
-                                Some(n) if n.is_zero() => {
-                                    painter.hline(
-                                        rect.x_range(),
-                                        rect.bottom(),
-                                        (LINE_WIDTH, Color32::GREEN),
-                                    );
-                                }
-                                _ => {
-                                    painter.rect(
-                                        rect,
-                                        0.0,
-                                        if self.background {
-                                            Color32::GREEN.linear_multiply(BG_MULTIPLY)
-                                        } else {
-                                            Color32::TRANSPARENT
-                                        },
-                                        (LINE_WIDTH, Color32::GREEN),
-                                    );
-                                }
-                            }
-                        }
-                    }
-                    if self.show_text {
-                        let text_min_rect = painter.text(
-                            Pos2::ZERO,
-                            Align2::RIGHT_BOTTOM,
-                            "+",
-                            FontId::monospace(self.signal_font_size),
-                            Color32::TRANSPARENT,
-                        );
-                        if rect.width() >= text_min_rect.width() + TEXT_ROUND_OFFSET {
-                            let pos = match self.align {
-                                SignalViewAlign::Left => {
-                                    rect.left_center() + vec2(TEXT_ROUND_OFFSET, 0.0)
-                                }
-                                SignalViewAlign::Center => {
-                                    rect.left_center() + vec2(width * percent_text, 0.0)
-                                }
-                                SignalViewAlign::Right => {
-                                    rect.right_center() - vec2(TEXT_ROUND_OFFSET, 0.0)
-                                }
-                            };
-                            // pre-paint to calculate size
-                            let text_rect = painter.text(
-                                pos,
-                                match self.align {
-                                    SignalViewAlign::Left => Align2::LEFT_CENTER,
-                                    SignalViewAlign::Center => Align2::CENTER_CENTER,
-                                    SignalViewAlign::Right => Align2::RIGHT_CENTER,
-                                },
-                                text.as_str(),
-                                FontId::monospace(self.signal_font_size),
-                                Color32::TRANSPARENT,
-                            );
-                            let paint_text =
-                                if rect.width() >= text_rect.width() + TEXT_ROUND_OFFSET {
-                                    text
-                                } else {
-                                    let text_mono_width = text_rect.width() / text.len() as f32;
-                                    let text_len = text.len();
-                                    let remains = &text[0..(text_len
-                                        - ((text_rect.width() + TEXT_ROUND_OFFSET - rect.width())
-                                            / text_mono_width)
-                                            as usize)];
-                                    if remains.len() <= 1 {
-                                        "+".to_string()
-                                    } else {
-                                        let len = remains.len();
-                                        format!("{}+", &remains[0..(len - 2)])
-                                    }
-                                };
-                            painter.text(
-                                pos,
-                                match self.align {
-                                    SignalViewAlign::Left => Align2::LEFT_CENTER,
-                                    SignalViewAlign::Center => Align2::CENTER_CENTER,
-                                    SignalViewAlign::Right => Align2::RIGHT_CENTER,
-                                },
-                                paint_text,
-                                // Default::default(),
-                                FontId::monospace(self.signal_font_size),
-                                text_color,
-                            );
-                        }
-                    }
-                }
-            } else {
-                // ignore this paint, record start pos
-                if ignore_x_start < 0.0 {
-                    ignore_x_start = rect.left();
-                }
-                if text.contains('x') || text.contains('z') {
-                    ignore_has_x = true;
-                }
-            }
-        };
-        while let Some(item) = it.next() {
-            if let Some(item_last) = item_last {
-                paint_signal(item_last, item);
-            }
-            item_last = Some(item);
-        }
-        if let Some(item_last) = item_last {
-            paint_signal(
-                item_last,
-                &WaveDataItem {
-                    timestamp: info.range.1,
-                    ..WaveDataItem::default()
-                },
-            );
-        }
-        // draw last
-        if ignore_x_start >= 0.0 {
-            painter.rect_filled(
-                Rect::from_x_y_ranges(
-                    RangeInclusive::new(ignore_x_start, signal_rect.right()),
-                    signal_rect.y_range(),
-                ),
-                0.0,
-                if ignore_has_x {
-                    Color32::DARK_RED
-                } else {
-                    Color32::GREEN
-                },
-            )
-        }
-        response
-    }
-    fn ui_signal_label(&self, signal: &SignalView, ui: &mut Ui) {
-        let text = signal.s.to_string();
-        ui.scope(|ui| {
-            ui.set_height(signal.height);
-            ui.centered_and_justified(|ui| {
-                ui.add(egui::Label::new(text).wrap(false));
-            });
-        });
-    }
     pub fn panel(&mut self, ui: &mut Ui, info: &Option<WaveInfo>, wave_data: &[WaveDataItem]) {
         if let Some(info) = info {
             if self.range.0 == 0 && self.range.1 == 0 {
                 self.range = info.range;
             }
         }
-        egui::TopBottomPanel::top("wave_top")
+        TopBottomPanel::top("wave_top")
             .resizable(false)
             .show_inside(ui, |ui| {
                 self.toolbar(ui);
@@ -351,7 +93,7 @@ impl WaveView {
             .striped(true)
             .resizable(false)
             // .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
-            .cell_layout(egui::Layout::centered_and_justified(Direction::TopDown))
+            .cell_layout(Layout::centered_and_justified(Direction::TopDown))
             .column(Column::exact(fix_width).resizable(false))
             .column(Column::exact(self.wave_width).resizable(false));
         // .column(Column::auto())
