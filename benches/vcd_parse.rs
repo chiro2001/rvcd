fc9e472063779c4f6d88a7f20c9047c8e752daf3
Chiro Liang <Chiro2001@163.com>
2023-02-03 16:02:34 +0800

    fix benchmark

--- benches/vcd_parse.rs
+++ benches/vcd_parse.rs
@@ -5,28 +5,28 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rvcd::wave::vcd_parser::Vcd;
 use rvcd::wave::WaveLoader;
 use std::fs::File;
-use tracing::warn;
+use tracing::{info, warn};
 
-fn main() {
-    fn optimize_vcd_parser(path: &str) -> Result<()> {
-        tracing_subscriber::fmt::init();
-        if let Ok(mut input) = File::open(path) {
-            Vcd::load(&mut input)?;
-        } else {
-            warn!("file not found: {}", path);
-        }
-        Ok(())
+fn optimize_vcd_parser(path: &str) -> Result<()> {
+    info!("optimize_vcd_parser({})", path);
+    if let Ok(mut input) = File::open(path) {
+        Vcd::load(&mut input)?;
+    } else {
+        warn!("file not found: {}", path);
     }
-
-    fn criterion_benchmark(c: &mut Criterion) {
-        let files = ["data/testbench.vcd"];
-        for file in files {
-            c.bench_function(format!("load {}", file).as_str(), |b| {
-                b.iter(|| black_box(optimize_vcd_parser(file)))
-            });
-        }
-    }
-
-    criterion_group!(benches, criterion_benchmark);
-    criterion_main!(benches);
+    Ok(())
 }
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let files = ["data/testbench.vcd"];
+    for file in files {
+        let id = format!("load {}", file);
+        println!("id: {}", id);
+        c.bench_function(id.as_ref(), |b| {
+            b.iter(|| black_box(optimize_vcd_parser(file)))
+        });
+    }
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
