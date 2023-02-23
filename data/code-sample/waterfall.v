7e05705271f97f49d5d0a9b12f9804a358a68f2d
Chiro Liang <Chiro2001@163.com>
2023-02-23 21:14:02 +0800

    add code example

---
+++ data/code-sample/waterfall.v
@@ -0,0 +1,43 @@
+module waterfall (
+    input wire clk,
+    input wire rst,
+    input wire start,
+    input wire [1:0] freq_set,
+    output reg [7:0] led
+);
+
+reg [31:0] clock_cnt_limit;
+reg [31:0] clock_cnt;
+reg has_started;
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        led <= 8'h01;
+        clock_cnt <= 32'h0000_0001;
+        has_started <= 1'b0;
+
+        case (freq_set)
+            2'b00:  clock_cnt_limit <= 32'd1_0000000;   // 1 cycle
+            2'b01:  clock_cnt_limit <= 32'd2_0000000;   // 2 cycle
+            2'b10:  clock_cnt_limit <= 32'd5_0000000;   // 5 cycle
+            2'b11:  clock_cnt_limit <= 32'd10_0000000;   // 10 cycle
+            default: clock_cnt_limit <= 32'h0000_0000;
+        endcase
+    end else begin
+        if (start) begin
+            has_started <= 1'b1;
+        end
+
+        if (has_started | start) begin
+            if (clock_cnt == clock_cnt_limit) begin
+                clock_cnt <= 32'h0000_0001;  
+                led <= led == 8'h80 ? 8'h01 : led << 8'h1;
+            end else begin
+                clock_cnt <= clock_cnt + 32'h1; 
+                led <= led;
+            end
+        end 
+    end
+end
+
+endmodule
---
+++ data/code-sample/waterfall_tb.v
@@ -0,0 +1,63 @@
+`timescale 1ns/1ps
+
+module tb_waterfall ();
+    
+reg clk;
+reg rst;
+reg start;
+reg [1:0] freq_set;
+wire [7:0] led;
+
+waterfall u_waterfall(
+    .clk(clk),
+    .rst(rst),
+    .start(start),
+    .freq_set(freq_set),
+    .led(led)
+);
+
+always #5 clk = ~clk;
+
+initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, clk, rst, start, freq_set, led);
+    
+    clk = 1'b0;
+
+    #0  rst = 1'b0; start = 1'b0; freq_set = 2'b00;
+
+    // test for 1 cycles, #10 * 16 == 160
+    #10 rst = 1'b1; start = 1'b0; freq_set = 2'b00;
+    #5  rst = 1'b0; start = 1'b1; 
+    #5  start = 1'b0;
+    #160;
+
+    // test for 2 cycles, #20 * 16 == 320
+    #10 rst = 1'b1; start = 1'b0; freq_set = 2'b01;
+    #5  rst = 1'b0; start = 1'b1; 
+    #5  start = 1'b0;
+    #320;
+
+    // test for 5 cycles, #50 * 8 = 400
+    #10 rst = 1'b1; start = 1'b0; freq_set = 2'b10;
+    #5  rst = 1'b0; start = 1'b1; 
+    #5  start = 1'b0;
+    #400;
+
+    // test for 10 cycles, #100 * 8 = 800
+    #10 rst = 1'b1; start = 1'b0; freq_set = 2'b11;
+    #5  rst = 1'b0; start = 1'b1; 
+    #5  start = 1'b0;
+    #800;
+
+    // for not start
+    #10 rst = 1'b1; freq_set = 2'b01;
+    #5  rst = 1'b0;
+    #50 start = 1'b1;
+    #5  start = 1'b0;
+    #200;
+
+    #10 $finish;
+end
+
+endmodule
