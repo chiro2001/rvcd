3ad88c3581323dda9ee7bef0c7eafe4208550967
Chiro Liang <Chiro2001@163.com>
2023-02-25 19:54:57 +0800

    VerilogModulesVisitor

--- src/verilog/mod.rs
+++ src/verilog/mod.rs
@@ -3,7 +3,25 @@ mod verilogparser;
 mod verilogparservisitor;
 mod verilogparserlistener;
 
+use antlr_rust::tree::{ParseTree, ParseTreeVisitorCompat};
 pub use veriloglexer::*;
 pub use verilogparser::*;
 pub use verilogparservisitor::*;
 pub use verilogparserlistener::*;
+
+pub struct VerilogModulesVisitor(pub Vec<String>);
+
+impl<'i> ParseTreeVisitorCompat<'i> for VerilogModulesVisitor {
+    type Node = VerilogParserContextType;
+    type Return = Vec<String>;
+
+    fn temp_result(&mut self) -> &mut Self::Return {
+        &mut self.0
+    }
+}
+
+impl <'i> VerilogParserVisitorCompat<'i> for VerilogModulesVisitor {
+    fn visit_module_declaration(&mut self, ctx: &Module_declarationContext<'i>) -> Self::Return {
+        vec![ctx.get_text()]
+    }
+}
--- tests/code_parser.rs
+++ tests/code_parser.rs
@@ -1,27 +1,10 @@
 use antlr_rust::InputStream;
-use rvcd::verilog::{Module_declarationContext, VerilogLexer, VerilogParser, VerilogParserContextType, VerilogParserVisitorCompat};
 use std::io::Read;
 use antlr_rust::common_token_stream::CommonTokenStream;
 use antlr_rust::token_factory::CommonTokenFactory;
-use antlr_rust::tree::{ParseTree, ParseTreeVisitorCompat, TerminalNode, VisitChildren};
+use antlr_rust::tree::ParseTreeVisitorCompat;
 use tracing::info;
-
-struct MyVerilogVisitor(Vec<String>);
-
-impl<'i> ParseTreeVisitorCompat<'i> for MyVerilogVisitor {
-    type Node = VerilogParserContextType;
-    type Return = Vec<String>;
-
-    fn temp_result(&mut self) -> &mut Self::Return {
-        &mut self.0
-    }
-}
-
-impl <'i> VerilogParserVisitorCompat<'i> for MyVerilogVisitor {
-    fn visit_module_declaration(&mut self, ctx: &Module_declarationContext<'i>) -> Self::Return {
-        vec![ctx.get_text()]
-    }
-}
+use rvcd::verilog::{VerilogModulesVisitor, VerilogLexer, VerilogParser};
 
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
@@ -34,7 +17,7 @@ fn main() -> anyhow::Result<()> {
     let token_source = CommonTokenStream::new(lexer);
     let mut parser = VerilogParser::new(token_source);
     let result = parser.source_text().expect("parsed unsuccessfully");
-    let mut visitor = MyVerilogVisitor(Vec::new());
+    let mut visitor = VerilogModulesVisitor(Vec::new());
     let visitor_result = visitor.visit(&*result);
     println!("modules: {:?}", visitor_result);
     Ok(())
