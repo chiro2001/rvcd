19194ee87b14a95eed5e967cc1a87edbd5b96466
Chiro Liang <Chiro2001@163.com>
2023-03-01 09:25:22 +0800

    update release.sh

--- release.sh
+++ release.sh
@@ -1,7 +1,9 @@
 #/bin/sh
 cargo build --release && \
-cp target/release/rvcd ../scaleda/src/main/resources/bin && \
-upx ../scaleda/src/main/resources/bin/rvcd && \
+cp target/release/rvcd release/ && \
+upx release/rvcd && \
 cargo build --release --target=x86_64-pc-windows-gnu && \
-cp target/x86_64-pc-windows-gnu/release/rvcd.exe ../scaleda/src/main/resources/bin && \
-upx ../scaleda/src/main/resources/bin/rvcd.exe
+cp target/x86_64-pc-windows-gnu/release/rvcd.exe release/ && \
+upx release/rvcd.exe
+rm -rf release.zip
+7z a release.zip -r release/
