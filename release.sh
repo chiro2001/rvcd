db384f0aaa2e60d7379fc54c080d0357fa177149
Chiro Liang <Chiro2001@163.com>
2023-03-01 23:57:34 +0800

    update release.sh

--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@ upx release/rvcd && \
 cargo build --release --target=x86_64-pc-windows-gnu && \
 cp target/x86_64-pc-windows-gnu/release/rvcd.exe release/ && \
 upx release/rvcd.exe
+trunk build --release && cp -r dist/ release/
 rm -rf release.zip && \
 cd release/ && 7z a ../release.zip -r * && cd ..
 cd release/ && 7z a ../../scaleda/src/main/resources/bin/assets.zip -r * && cd ..
