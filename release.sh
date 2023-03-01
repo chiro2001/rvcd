d8c9f714c201e397178955b8119f1321c446f551
Chiro Liang <Chiro2001@163.com>
2023-03-01 09:34:11 +0800

    update release.sh

--- release.sh
+++ release.sh
@@ -5,5 +5,5 @@ upx release/rvcd && \
 cargo build --release --target=x86_64-pc-windows-gnu && \
 cp target/x86_64-pc-windows-gnu/release/rvcd.exe release/ && \
 upx release/rvcd.exe
-rm -rf release.zip
-7z a release.zip -r release/
+rm -rf release.zip && \
+cd release/ && 7z a ../release.zip -r * && cd ..
