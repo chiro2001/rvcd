8a1dab555b58eef1b68769f3a8778538234beb90
Chiro Liang <Chiro2001@163.com>
2023-03-01 16:52:49 +0800

    update release.sh

--- release.sh
+++ release.sh
@@ -7,4 +7,4 @@ cp target/x86_64-pc-windows-gnu/release/rvcd.exe release/ && \
 upx release/rvcd.exe
 rm -rf release.zip && \
 cd release/ && 7z a ../release.zip -r * && cd ..
-cd release/ && 7z a ../scaleda/src/main/resources/bin/assets.zip -r * && cd ..
+cd release/ && 7z a ../../scaleda/src/main/resources/bin/assets.zip -r * && cd ..
