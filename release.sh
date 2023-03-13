01f42e93925289d59e22981189c2ccf26d0bc282
Chiro Liang <Chiro2001@163.com>
2023-03-13 13:31:27 +0800

    update release script

--- release.sh
+++ release.sh
@@ -1,11 +1,19 @@
 #/bin/sh
-cargo build --release && \
-cp target/release/rvcd release/ && \
-upx release/rvcd && \
-cargo build --release --target=x86_64-pc-windows-gnu && \
-cp target/x86_64-pc-windows-gnu/release/rvcd.exe release/ && \
-upx release/rvcd.exe
-trunk build --release && cp -r dist/ release/
-rm -rf release.zip && \
+if [[ -v BUILD ]]
+then
+  cargo build --release && \
+  cp target/release/rvcd release/ && \
+  upx release/rvcd && \
+  cargo build --release --target=x86_64-pc-windows-gnu && \
+  cp target/x86_64-pc-windows-gnu/release/rvcd.exe release/ && \
+  upx release/rvcd.exe
+fi
+# trunk build --release && cp -r dist/ release/
+rm -rf release.zip
+if [[ -z DELETE_ASSETS ]]
+then
+  rm ../../scaleda/src/main/resources/bin/assets.zip
+fi
+# other asserts in release/ will also packed
 cd release/ && 7z a ../release.zip -r * && cd ..
 cd release/ && 7z a ../../scaleda/src/main/resources/bin/assets.zip -r * && cd ..
