68cd82a79639ec9a93737c9f41e7be107f4bee32
Chiro Liang <Chiro2001@163.com>
2024-04-30 09:47:51 +0800

    fixing execution

--- src/main.rs
+++ src/main.rs
@@ -36,115 +36,119 @@ struct RvcdArgs {
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
-#[tokio::main]
-async fn main() -> Result<()> {
+// #[tokio::main]
+fn main() -> Result<()> {
     use rvcd::{
         app,
         manager::{RvcdExitMessage, RvcdManager},
         rpc::RvcdInputEvent,
     };
     use std::sync::{Arc, Mutex};
+    use tokio::runtime::Builder;
     use tonic::transport::Server;
     use tracing::error;
 
-    let args = RvcdArgs::parse();
+    let rt = Builder::new_multi_thread().enable_all().build().unwrap();
+    rt.block_on(async {
+        let args = RvcdArgs::parse();
 
-    // Log to stdout (if you run with `RUST_LOG=debug`).
-    tracing_subscriber::fmt::init();
+        // Log to stdout (if you run with `RUST_LOG=debug`).
+        tracing_subscriber::fmt::init();
 
-    app::init();
+        app::init();
 
-    // let native_options = eframe::NativeOptions::default();
-    let native_options = eframe::NativeOptions {
-        // drag_and_drop_support: true,
-        // depth_buffer: 3,
-        // initial_window_size: Some([1280.0, 1024.0].into()),
-        // #[cfg(feature = "wgpu")]
-        // renderer: eframe::Renderer::Wgpu,
-        ..Default::default()
-    };
-    let (rpc_tx, rpc_rx) = mpsc::channel();
-    let rpc_tx2 = rpc_tx.clone();
-    let rpc_tx3 = rpc_tx.clone();
-    let rpc_tx4 = rpc_tx.clone();
-    let rpc_tx5 = rpc_tx.clone();
-    let (manager_tx, manager_rx) = mpsc::channel();
-    let (exit_tx, exit_rx) = mpsc::channel();
-    let exit_tx2 = exit_tx.clone();
-    let src = args.src.clone();
-    let gui = async move {
-        eframe::run_native(
-            "Rvcd",
-            native_options,
-            Box::new(|cc| {
-                Box::new(RvcdApp::new(
-                    cc,
-                    rpc_rx,
-                    rpc_tx2,
-                    manager_tx,
-                    exit_tx2,
-                    if src.is_empty() { None } else { Some(src) },
-                ))
-            }),
-        )
-        .expect("gui panic!");
-    };
-    let rpc = async move {
-        let manager_rx = Arc::new(Mutex::new(manager_rx));
-        loop {
-            let rpc_tx = rpc_tx.clone();
-            let exit_tx = exit_tx.clone();
-            let addr = format!("0.0.0.0:{}", args.port).parse().unwrap();
-            info!("[Manager] rpc server at {}", addr);
-            let reflection_service = tonic_reflection::server::Builder::configure()
-                .register_encoded_file_descriptor_set(rvcd::rpc::RVCD_FILE_DESCRIPTOR_SET)
-                .build()
-                .unwrap();
-            match Server::builder()
-                .add_service(reflection_service)
-                .add_service(rvcd::rpc::rvcd_rpc_server::RvcdRpcServer::new(
-                    RvcdManager::new(rpc_tx, manager_rx.clone(), exit_tx),
-                ))
-                .serve(addr)
-                .await
-            {
-                Ok(_) => {}
-                Err(e) => error!("cannot run rpc: {:?}", e),
+        // let native_options = eframe::NativeOptions::default();
+        let native_options = eframe::NativeOptions {
+            // drag_and_drop_support: true,
+            // depth_buffer: 3,
+            // initial_window_size: Some([1280.0, 1024.0].into()),
+            // #[cfg(feature = "wgpu")]
+            // renderer: eframe::Renderer::Wgpu,
+            ..Default::default()
+        };
+        let (rpc_tx, rpc_rx) = mpsc::channel();
+        let rpc_tx2 = rpc_tx.clone();
+        let rpc_tx3 = rpc_tx.clone();
+        let rpc_tx4 = rpc_tx.clone();
+        let rpc_tx5 = rpc_tx.clone();
+        let (manager_tx, manager_rx) = mpsc::channel();
+        let (exit_tx, exit_rx) = mpsc::channel();
+        let exit_tx2 = exit_tx.clone();
+        let src = args.src.clone();
+        let gui = async move {
+            eframe::run_native(
+                "Rvcd",
+                native_options,
+                Box::new(|cc| {
+                    Box::new(RvcdApp::new(
+                        cc,
+                        rpc_rx,
+                        rpc_tx2,
+                        manager_tx,
+                        exit_tx2,
+                        if src.is_empty() { None } else { Some(src) },
+                    ))
+                }),
+            )
+            .expect("gui panic!");
+        };
+        let rpc = async move {
+            let manager_rx = Arc::new(Mutex::new(manager_rx));
+            loop {
+                let rpc_tx = rpc_tx.clone();
+                let exit_tx = exit_tx.clone();
+                let addr = format!("0.0.0.0:{}", args.port).parse().unwrap();
+                info!("[Manager] rpc server at {}", addr);
+                let reflection_service = tonic_reflection::server::Builder::configure()
+                    .register_encoded_file_descriptor_set(rvcd::rpc::RVCD_FILE_DESCRIPTOR_SET)
+                    .build()
+                    .unwrap();
+                match Server::builder()
+                    .add_service(reflection_service)
+                    .add_service(rvcd::rpc::rvcd_rpc_server::RvcdRpcServer::new(
+                        RvcdManager::new(rpc_tx, manager_rx.clone(), exit_tx),
+                    ))
+                    .serve(addr)
+                    .await
+                {
+                    Ok(_) => {}
+                    Err(e) => error!("cannot run rpc: {:?}", e),
+                }
+                match exit_rx.try_recv() {
+                    Ok(msg) => match msg {
+                        RvcdExitMessage::Exit => break,
+                    },
+                    _ => {}
+                }
+                sleep_ms(1000).await;
             }
-            match exit_rx.try_recv() {
-                Ok(msg) => match msg {
-                    RvcdExitMessage::Exit => break,
-                },
-                _ => {}
-            }
-            sleep_ms(1000).await;
+        };
+        for file in args.file {
+            rpc_tx3.send(RvcdRpcMessage::OpenWaveFile(file)).unwrap();
         }
-    };
-    for file in args.file {
-        rpc_tx3.send(RvcdRpcMessage::OpenWaveFile(file)).unwrap();
-    }
-    for source in args.input {
-        rpc_tx3
-            .send(RvcdRpcMessage::OpenSourceFile(source))
-            .unwrap();
-    }
-    tokio::spawn(rpc);
-    tokio::spawn(RvcdApp::frame_buffer_tcp_server(
-        rvcd::manager::DISP_PORT,
-        rpc_tx4,
-    ));
-    if args.hidden {
-        let mut event = RvcdInputEvent::default();
-        event.set_type(rvcd::rpc::EventType::Visible);
-        event.data = 0;
-        rpc_tx5.send(RvcdRpcMessage::InputEvent(event)).unwrap();
-    }
-    #[cfg(target_os = "linux")]
-    tokio::spawn(RvcdApp::frame_buffer_unix_server(
-        rvcd::manager::UNIX_FB_PATH,
-        rpc_tx5,
-    ));
-    gui.await;
+        for source in args.input {
+            rpc_tx3
+                .send(RvcdRpcMessage::OpenSourceFile(source))
+                .unwrap();
+        }
+        tokio::spawn(rpc);
+        tokio::spawn(RvcdApp::frame_buffer_tcp_server(
+            rvcd::manager::DISP_PORT,
+            rpc_tx4,
+        ));
+        if args.hidden {
+            let mut event = RvcdInputEvent::default();
+            event.set_type(rvcd::rpc::EventType::Visible);
+            event.data = 0;
+            rpc_tx5.send(RvcdRpcMessage::InputEvent(event)).unwrap();
+        }
+        #[cfg(target_os = "linux")]
+        tokio::spawn(RvcdApp::frame_buffer_unix_server(
+            rvcd::manager::UNIX_FB_PATH,
+            rpc_tx5,
+        ));
+        gui.await;
+    });
     Ok(())
 }
 
--- src/utils.rs
+++ src/utils.rs
@@ -4,7 +4,14 @@ use std::future::Future;
 #[cfg(not(target_arch = "wasm32"))]
 pub fn execute<F: Future<Output = ()> + Send + 'static>(f: F) {
     // this is stupid... use any executor of your choice instead
-    std::thread::spawn(move || futures::executor::block_on(f));
+    // std::thread::spawn(move || futures::executor::block_on(f));
+    std::thread::spawn(move || {
+        let rt = tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        rt.block_on(f);
+    });
 }
 #[cfg(target_arch = "wasm32")]
 pub fn execute<F: Future<Output = ()> + 'static>(f: F) {
@@ -13,7 +20,8 @@ pub fn execute<F: Future<Output = ()> + 'static>(f: F) {
 
 pub async fn sleep_ms(mills: u64) {
     #[cfg(not(target_arch = "wasm32"))]
-    std::thread::sleep(std::time::Duration::from_millis(mills));
+    // std::thread::sleep(std::time::Duration::from_millis(mills));
+    tokio::time::sleep(std::time::Duration::from_millis(mills)).await;
     #[cfg(target_arch = "wasm32")]
     {
         // #[wasm_bindgen]
